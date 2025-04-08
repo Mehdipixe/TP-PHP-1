6e190be72d114ea15ad8fcb994cb30f4c406a4c1
Mehdipixe <mehdiahnou@gmail.com>
2025-04-08 09:34:56 +0200

    bouclefor

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php 
 
-// Ex 1
+//Ex 1
 
-// for ($i =0; $i <= 10; $i++) {
-   // echo $i;
-// }
+for ($i =0; $i <= 10; $i++) {
+   echo $i;
+}
 
 // Ex 2
 
-// $somme = 0;
-// for ($i = 1; $i <= 100; $i++){
-   // $somme += $i;
-// }
-// echo $somme;
+$somme = 0;
+for ($i = 1; $i <= 100; $i++){
+   $somme += $i;
+}
+echo $somme;
 
 //  Ex 3
 
-// $nombre = 5;
+$nombre = 5;
  
-// echo "Table de multiplication pour $nombre :/n";
+echo "Table de multiplication pour $nombre :/n";
 
-// for  ($i = 1; $i <= 10; $i++){
-   // $resultat = $nombre * $i;
-   // echo "$nombre x $i =$resultat\n";
-// }
+for  ($i = 1; $i <= 10; $i++){
+   $resultat = $nombre * $i;
+   echo "$nombre x $i =$resultat\n";
+}
 ?>
