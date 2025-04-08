c82f7173d7574c917babef0e809f4c0d34e951cc
Mehdipixe <mehdiahnou@gmail.com>
2025-04-08 09:59:07 +0200

    boucleforeach

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php 
+
+//  Ex 1
  
- for ($x  = 0; $x <= 10;  $x++) {
-    echo  "the number is: $x <br>";
+ $fruits = array("pomme","banane","orange","fraise","kiwi");
+ 
+ foreach ($fruits as $x) {
+   echo "$x <br>";
  }
+ 
+
+//  Ex 2
+
+$members =  array ("Dupont Jean "=>"30 ans ","ville"=>"Paris");
+
+foreach ($members as $x => $y) {
+   echo "$x : $y <br>";
+}
+
+//  Ex 3
+
+$utilisateurs = [
+   ['nom' => 'Martin', 'note' => '15'],
+   ['nom' => 'Durand', 'note' => '12'],
+   ['nom' => 'Petit', 'mail' => '18'],
+];
+
+foreach($utilisateurs as $nb => $infos){
+   echo 'Utilisateur n°' .($nb + 1). ' :<br>';
+   foreach ($infos as $c => $v){
+       echo $c. ' : ' .$v. '<br>';
+   }
+   echo '<\br>';
+}
+
+   
+
+
+
+
  ?>
