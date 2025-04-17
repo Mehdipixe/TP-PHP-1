504d70be0b2704a4114ea9699512efbd40a39c61
Mehdipixe <mehdiahnou@gmail.com>
2025-04-17 15:06:30 +0200

    classes

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-des comportements.
-<?php
+
 // Définition d'une classe Personne
 class Personne {
     // Propriétés (attributs)
@@ -10,19 +9,59 @@ class Personne {
     private $age;
     
     // Constructeur
+    public function __construct($nom,$prenom,$age){
+         $this-> SetNom($nom);
+         $this-> SetPrenom($prenom);
+         $this-> SetAge($age);
 
+    }
+  
     
     // Getters (accesseurs)
-    
+    public function getNom() {
+      return $this-> nom = $nom;
+    }
+    public function GetPrenom(){
+      return $this-> prenom = $prenom;
+    }
+    public function GetAge(){
+      return $this-> age = $age;
+    }
     // Setters (mutateurs)
+    public function SetNom($nom){
+          $this->nom = $nom;  
+    }
 
+    public function SetPrenom($prenom){
+          $this-> prenom = $prenom;
+    }
     
+    public function SetAge($age){
+          $this-> age=  $age;
+    }
     // Méthodes
-    public function sePresenter()
     
-    public function estMajeur()
-
+    
+    
+    public function sePresenter(){
+      echo "Bonjour je m'appelle ". $this->prenom . " ".$this->nom. " et j'ai " .$this->age ." ans. <br>";
+    }
+    public function estMajeur(){
+      if($this->age >= 18){
+        echo "Je suis majeur.</br>";
+      } else {
+        echo "Je suis majeur.</br>";
+      }
+    }
+  }
 // Utilisation de la classe
-
+$personne = new Personne("Ahnou", "Mehdi", 21);
+$personne->sePresenter();
+$personne->estMajeur();
 
 // Modification des propriétés
+$personne->setAge(19);
+$personne->setNom("Guillard");
+$personne->SetPrenom("Lény");
+$personne->sePresenter();
+$personne->estMajeur();
